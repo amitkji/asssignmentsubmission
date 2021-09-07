9eae50e338bcc5c88d6c898dfc06f7365ad67e36
amitkji <90267538+amitkji@users.noreply.github.com>
2021-09-07 20:22:30 +0530

    day 1 assignment

---
+++ day1.py
@@ -0,0 +1,24 @@
+#AUTHOR AMIT hash used for single line comment
+''' triple quotes used for multiline comment'''
+print("HI I AM AMIT")
+print('''u need to put triple quotes for 
+multiline string''')
+a="amit"
+b=123
+c=1.2
+d=10
+e=5
+print("the value of 10+5 is",10+5)
+print("the value of 10-5 is",10-5)
+print("the value of 10*5 is",10*5)
+print("the value of 10/5 is",10/5)
+d+=10
+print(d)
+d-=10
+print(d)
+d*=10
+print(d)
+d/=10
+print(d)
+d=2**10
+print(d)
